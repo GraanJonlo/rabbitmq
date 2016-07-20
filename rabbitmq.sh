a64427c7c819ded50f8ce11dd296d7ac432966b5
Andrew Grant <andrew.grant@codecomputerlove.com>
2016-07-20 10:54:25 +0100

    Trying to fix permissions problems

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -4,7 +4,7 @@
 
 chown rabbitmq:rabbitmq /var/log/rabbitmq
 chown rabbitmq:rabbitmq /var/lib/rabbitmq/mnesia
-chmod 0755 /var/log/rabbitmq
-chmod 0755 /var/lib/rabbitmq/mnesia
+chmod -R 0755 /var/log/rabbitmq
+chmod -R 0755 /var/lib/rabbitmq
 
 exec /sbin/setuser rabbitmq /usr/sbin/rabbitmq-server
